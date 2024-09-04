05b6af0659b6520e400eef4d09bde169c31c7bcd
Kacper <[k.kacper.korzeniewski@gmail.com]>
2024-09-04 17:30:19 +0200

    add events listeners for canva, butons and color picker

--- script.js
+++ script.js
@@ -6,17 +6,27 @@ const displayCanvaSize = document.querySelector('#canva-size');
 function updateCanvaSizeDisplay() {
     displayCanvaSize.textContent = `${slider.value} x ${slider.value}`;
 }
-const tempCanva = document.createElement('div');
+
+function createSquare(canvaSize){
+    const square = document.createElement('div');
+    square.classList.add('grid-square');
+    square.style.width = canva.clientWidth / canvaSize + 'px';
+    square.style.height = canva.clientHeight / canvaSize + 'px';
+
+    square.addEventListener('mouseover', () => {
+        square.style.backgroundColor = markerColor.value;
+    });
+
+    return square;
+}
+
 function createGrid(canvaSize) {   
     canva.textContent = '';
     for (let i = 0; i < canvaSize; i++) {
         const row = document.createElement('div');
         row.classList.add('grid-row');
         for (let j = 0; j < canvaSize; j++) {
-            const square = document.createElement('div');
-            square.classList.add('grid-square');
-            square.style.width = canva.clientWidth / canvaSize + 'px';
-            square.style.height = canva.clientHeight / canvaSize + 'px';
+            const square = createSquare(canvaSize);
             row.appendChild(square);
         }
         canva.appendChild(row);
@@ -30,6 +40,23 @@ slider.addEventListener('input', () =>{
     createGrid(slider.value);
 });
 
-const color = document.querySelector('#color');
+const markerColor = document.querySelector('#color');
+
+markerColor.addEventListener('input', () => {
+    canva.style.backgroundColor = markerColor.value;
+});
+
 const erase = document.querySelector('#erase');
+erase.addEventListener('click', () => {
+    markerColor.value = "#F0F8FF";
+});
+
+
 const clear = document.querySelector('#clear');
+clear.addEventListener('click', () => {
+    createGrid(slider.value);
+});
+
+window.addEventListener('resize', () => {
+    createGrid(slider.value); 
+});
